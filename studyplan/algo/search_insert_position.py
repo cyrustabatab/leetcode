7fb4b9f1af9ce1729b6983765a4fea51533516c2
Cyrus Taba <24766642+cyrustabatab@users.noreply.github.com>
2021-11-04 22:54:13 -0700

    arranging coins

---
+++ studyplan/algo/search_insert_position.py
@@ -0,0 +1,24 @@
+
+
+
+def search_insert_position(nums,target):
+
+
+    low,high = 0,len(nums) - 1
+
+
+    while low <= high:
+        mid = (low + high)//2
+
+        if a[mid] == target:
+            return mid
+
+
+        if target < a[mid]:
+            high = mid -1
+        else:
+            low = mid + 1
+            
+
+    return mid
+
